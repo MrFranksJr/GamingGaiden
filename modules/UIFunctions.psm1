02b72f5a2a53dd6da89d8d9165a52afce7bb50d4
Franky Jr Blondeel <frankyjr@me.com>
2026-04-19 16:00:36 +0200

    Update: remove status text from UI, add tooltips for status icons

--- modules/UIFunctions.psm1
+++ modules/UIFunctions.psm1
@@ -114,18 +114,18 @@ function RenderGameList() {
             $gamingPCs = $pcArray -join '<br/>'
         }
 
-        $statusUri = "<div>Finished</div><img class=`"game-status-icon icon-finished`" src=`".\resources\images\finished.svg`">"
+        $statusUri = "<img class=`"game-status-icon icon-finished`" src=`".\resources\images\finished.svg`">"
         if ($gameRecord.completed -eq 'FALSE') {
-            $statusUri = "<div>Playing</div><img class=`"game-status-icon icon-playing`" src=`".\resources\images\playing.svg`">"
+            $statusUri = "<img class=`"game-status-icon icon-playing`" src=`".\resources\images\playing.svg`">"
         }
         if ($gameRecord.status -eq 'dropped') {
-            $statusUri = "<div>Dropped</div><img class=`"game-status-icon icon-dropped`" title=`"Utter Garbage!!`" src=`".\resources\images\dropped.svg`">"
+            $statusUri = "<img class=`"game-status-icon icon-dropped`" src=`".\resources\images\dropped.svg`">"
         }
         if ($gameRecord.status -eq 'hold') {
-            $statusUri = "<div>On Hold</div><img class=`"game-status-icon icon-onhold`" src=`".\resources\images\hold.svg`">"
+            $statusUri = "<img class=`"game-status-icon icon-onhold`" src=`".\resources\images\hold.svg`">"
         }
         if ($gameRecord.status -eq 'forever') {
-            $statusUri = "<div>Forever</div><img class=`"game-status-icon icon-forever`" src=`".\resources\images\forever.svg`">"
+            $statusUri = "<img class=`"game-status-icon icon-forever`" src=`".\resources\images\forever.svg`">"
         }
 
         $currentGame = [Game]::new($iconUri, $name, $gameRecord.play_time, $gameRecord.session_count, $statusUri, $gameRecord.last_play_date, $gamingPCs)
